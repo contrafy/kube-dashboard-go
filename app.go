f2e8c46e1528f342c719e5f95472c182244e07dc
contrafy <araaiyan@tamu.edu>
2025-06-25 17:38:56 -0500

    cluster view

--- app.go
+++ app.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"context"
-	"fmt"
 )
 
 // App struct
 type App struct {
-	ctx  context.Context
-	kube *KubeService
+	ctx      context.Context
+	clusters map[string]*ClusterInfo
 }
 
 // NewApp creates a new App application struct
 func NewApp() *App {
-	return &App{}
+	return &App{
+		clusters: make(map[string]*ClusterInfo),
+	}
 }
 
 // startup is called when the app starts. The context is saved
@@ -21,18 +22,6 @@ func NewApp() *App {
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 
-	ks, err := NewKubeService() // default kubeconfig resolution
-	if err != nil {
-		// Bubble the error to the frontend later; for now just log
-		fmt.Printf("KubeService init error: %v\n", err)
-	} else {
-		a.kube = ks
-	}
-}
-
-func (a *App) GetNamespaces() ([]string, error) {
-	if a.kube == nil {
-		return nil, fmt.Errorf("kube service not initialised or kubeconfig not found")
-	}
-	return a.kube.GetNamespaces(a.ctx)
+	a.loadExistingClusters()
+	a.maybeCopyDefaultConfig()
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
 import {SetStateAction, useState} from 'react';
 import './App.css';
 
-import { Button } from "@/components/ui/button"
-
-
-import {GetNamespaces} from "../wailsjs/go/main/App";
+import Clusters from './views/Clusters';
 
 function App() {
-    const [errorText, setErrorText] = useState('');
-    const [namespaces, setNamespaces] = useState<string[]>([])
-
-    function loadNamespaces() {
-        GetNamespaces().then(setNamespaces).catch((err: SetStateAction<string>) => setErrorText(err))
-    }
-
     return (
         <div id="App">
-            <ul>{namespaces.map(ns => <li key={ns}>{ns}</li>)}</ul>
-            <Button className="btn" onClick={loadNamespaces}>Get Namespaces</Button>
-            <div id="error">
-                {errorText}
-            </div>
+            <Clusters />
         </div>
     )
 }
